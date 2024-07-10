8b0976d47c35bff5aa6da8a4e61fae46c709434e
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-10 19:55:01 +0530

    Final commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '32035dc3f3msh5be2ca1e23f445cp1b2af5jsn4dc00b2c54e3',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
+  
